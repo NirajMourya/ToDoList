78ab7119a57a3f3c2f37ebcb391bca8cc70b9130
NirajMourya <nirajmourya786@gmail.com>
2024-01-21 23:24:44 +0530

    created basic to do app

---
+++ todolist/src/components/Form.jsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+
+
+export default function Form(props){
+    const [name, setName] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        name.trim() === '' ? alert('Please enter a task name') : props.addTask(name);
+        setName("");
+    }
+    const handleChange = (e) => {
+        setName(e.target.value);
+    }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="new-todo-input"
+          name="text"
+          value={name}
+          onChange={handleChange}
+          className = "newToDo"
+          placeholder="Please add your tasks"
+        />
+        <button type="submit" class="btn btn-primary addtodo">
+          Add
+        </button>
+      </form>
+    )
+}
